f523f3460d068f232e68615ef16d231b68fd2c34
Jenny <jennypavlova94@gmail.com>
2016-12-15 20:04:43 +0200

    angular user controller wip

--- app/routes/user.js
+++ app/routes/user.js
@@ -6,7 +6,7 @@ var User = require('../models/user');
 module.exports = function(router) {
 
   passport.use(new LocalStrategy(User.authenticate()));
-
+  // route to log in 
   router.post('/login', passport.authenticate('local', {
     successRedirect: '/portfolio',
     failureRedirect: '/login',
@@ -22,32 +22,13 @@ module.exports = function(router) {
         return next();
       });
   });
-    // create a user (accessed at POST http://localhost:8080/api/user)
-  // router.post('/users', function(req, res) {
-  //   var user = new User({ 
-  //     name:{
-  //       first: "name",
-  //       last: "lastname"
-  //     },
-  //       username: "name",
-  //       password: "123"
-  //   });
-  //   user.first = req.body.first;  // set the users name (comes from the request)
-  //   user.last = req.body.last;  // set the users name (comes from the request)
-
-  //   // save the user and check for errors
-  //   user.save(function(err) {
-  //     if (err)
-  //       res.send(err); 
-  //     res.json({ message: 'User created!' });
-  //   })
-  // })     
-  // router.get('/users', function(req, res) {
-  //   User.find(function(err, user) {
-  //     if (err)
-  //       res.send(err);
-  //     res.json(user);
-  //   })
-  // })
-
+  // route to test if the user is logged in or not 
+  router.get('/loggedin', function(req, res) { 
+    res.send(req.isAuthenticated() ? req.user : '0'); 
+  }); 
+  // route to log out 
+  router.post('/logout', function(req, res){ 
+    req.logOut(); 
+    res.send(200); 
+  });
 }
