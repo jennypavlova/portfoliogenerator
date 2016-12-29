1b58eab81934b525e07b7a0c3e735738d3034025
Jenny <jennypavlova94@gmail.com>
2016-12-29 17:10:26 +0200

    Refactoring of portfolio model

--- app/models/portfolio.js
+++ app/models/portfolio.js
@@ -1,7 +1,22 @@
 var mongoose = require('mongoose');
 
 var portfolioschema = mongoose.Schema({
-    project : String
+    project : {
+    	name: {
+    		type: String,
+    		required: true
+    	},
+    	companyName: {
+    		type: String,
+    		required: true
+    	},
+    	projectDescription: String,
+    	duration: {
+    		startDate: Date,
+    		endDate: Date
+    	},
+    	technologies: String
+    }
 });
 
 module.exports = mongoose.model('Portfolio', portfolioschema);
